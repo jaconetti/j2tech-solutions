35d1d57d71cb38dbf8c6c794aac73fdceba35742
Jeferson Jaconetti <31043216+jaconetti@users.noreply.github.com>
2026-01-19 18:13:56 -0300

    fix: remove outputFileTracingRoot causing deployment errors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const path = require('path');
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
